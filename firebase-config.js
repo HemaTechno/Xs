676958444a708983299a712eb01b5059587d4a5f
HemaTechno <ibrahimmansour4655@gmail.com>
2025-12-31 19:12:51 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+<!-- firebase-config.js -->
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+  const firebaseConfig = {
+  apiKey: "AIzaSyBoPJbx5v6EkOqxOJkbhzHqIJdAByh79Rg",
+  authDomain: "hhhhhh-d4fb8.firebaseapp.com",
+  databaseURL: "https://hhhhhh-d4fb8-default-rtdb.firebaseio.com",
+  projectId: "hhhhhh-d4fb8",
+  storageBucket: "hhhhhh-d4fb8.appspot.com",
+  messagingSenderId: "24512338206",
+  appId: "1:24512338206:web:dfe045db59bd3434a2110f",
+  measurementId: "G-HD4R7GNQ5H"
+};
+
+  export const app = initializeApp(firebaseConfig);
+  export const db = getFirestore(app);
+</script>
