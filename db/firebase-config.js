b658c67f94d18e61771630d5c6bef247b70657f1
HemaTechno <ibrahimmansour4655@gmail.com>
2025-12-31 19:22:03 +0200

    Update firebase-config.js

--- db/firebase-config.js
+++ db/firebase-config.js
@@ -1,18 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-  const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyBoPJbx5v6EkOqxOJkbhzHqIJdAByh79Rg",
   authDomain: "hhhhhh-d4fb8.firebaseapp.com",
-  databaseURL: "https://hhhhhh-d4fb8-default-rtdb.firebaseio.com",
   projectId: "hhhhhh-d4fb8",
   storageBucket: "hhhhhh-d4fb8.appspot.com",
   messagingSenderId: "24512338206",
-  appId: "1:24512338206:web:dfe045db59bd3434a2110f",
-  measurementId: "G-HD4R7GNQ5H"
+  appId: "1:24512338206:web:dfe045db59bd3434a2110f"
 };
 
-  export const app = initializeApp(firebaseConfig);
-  export const db = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
+// Firestore
+const db = getFirestore(app);
+
+export { app, db };
