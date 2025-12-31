1af548e0f89bc3bd72ad49c339049a43f7ae7c7c
HemaTechno <ibrahimmansour4655@gmail.com>
2025-12-31 19:16:17 +0200

    Update and rename firebase-config.js to db/firebase-config.js

--- db/firebase-config.js
+++ db/firebase-config.js
@@ -1,4 +1,4 @@
-<!-- firebase-config.js -->
+
 <script type="module">
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
   import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
